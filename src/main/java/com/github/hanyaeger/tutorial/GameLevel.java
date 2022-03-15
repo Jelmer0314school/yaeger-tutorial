9a2415aa38f6e3f151849f0e01887cc0263fbc3e
Jelmer0314school <j.noppert@student.han.nl>
2022-03-15 11:14:18 +0100

    alles tot error

---
+++ src/main/java/com/github/hanyaeger/tutorial/GameLevel.java
@@ -0,0 +1,45 @@
+package com.github.hanyaeger.tutorial;
+
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.scenes.DynamicScene;
+import com.github.hanyaeger.tutorial.entities.Hanny;
+import com.github.hanyaeger.tutorial.entities.Swordfish;
+import javafx.scene.Cursor;
+
+public class GameLevel extends DynamicScene {
+    @Override
+    public void setupScene() {
+        setBackgroundAudio("audio/waterworld.mp3");
+        setBackgroundImage("backgrounds/background2.jpg");
+    }
+
+    @Override
+    public void setupEntities() {
+        var swordFish = new Swordfish(new Coordinate2D(getWidth() / 2, getHeight() / 2));
+        var hanny =  new Hanny(new Coordinate2D(getWidth() / 2, getHeight() / 2));
+        addEntity(swordFish);
+        addEntity(hanny);
+
+
+    }
+
+    @Override
+    public void setCursor(Cursor cursor) {
+        super.setCursor(cursor);
+    }
+
+    @Override
+    public double getWidth() {
+        return super.getWidth();
+    }
+
+    @Override
+    public double getHeight() {
+        return super.getHeight();
+    }
+
+    @Override
+    public void beforeInitialize() {
+        super.beforeInitialize();
+    }
+}
